739c61c9b5da8eccddbaca0be0d8481240652a85
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2022-10-24 13:06:32 +0600

    Add files via upload

---
+++ PHP/week 5/sessiontrack.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <title>HTML Site</title>
+  </head>
+  <body>
+    <table
+      border="1"
+      align="center"
+      width="800px"
+      style="border-collapse: collapse; margin-top: 20px"
+    >
+      <tr>
+        <td><img src="img/original-7c71bfa677e61dea61bc2acd59158d32.webp" alt="Company Logo" style="width: 80px" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="home.php" style="width: 100px" style="margin: 40px">Home</a> | <a href="login.php">Login</a> | <a href="register.php">Registration</a></td>
+     <br>
+    </tr>
+
+      <tr>
+        <td style="padding-bottom: 100px"><h2 style="margin: 30px">Welcome to xCompany</h2></td>
+      </tr>
+
+      <tr align="center">
+        <td>
+          <footer style="margin: 10px">Copyright &copy; 2017</footer>
+        </td>
+      </tr>
+
+    </table>
+
+  </body>
+</html>
+
+<?php
+
+?>

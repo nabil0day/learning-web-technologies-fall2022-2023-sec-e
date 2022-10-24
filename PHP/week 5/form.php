5a67c2e0055cf2e3653efbd391e26cd094008387
Hadiur Rahman Nabil <hadiurrahmannabil@gmail.com>
2022-10-24 13:53:29 +0600

    Add files via upload

---
+++ PHP/week 5/form.php
@@ -0,0 +1,89 @@
+<html>
+  <head>
+    <title>HTML Site</title>
+  </head>
+  <body>
+    <table
+      border="1"
+      align="center"
+      width="800px"
+      style="border-collapse: collapse; margin-top: 20px"
+    >
+      <tr>
+        <td><img src="img/images.png" alt="Company Logo" style="width: 100px" />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="home.php" style="width: 100px" style="margin: 40px">Home</a> | <a href="login.php">Login</a> | <a href="register.php">Registration</a></td>
+     <br>
+     <br>
+     <br>
+    </tr>
+
+      <tr>
+        <td 
+        <form method="" action="" enctype="">
+        <fieldset>
+            <legend>Registration</legend>
+            <table>
+            <tr>
+                    <td> Name </td>
+                    <td> <input type="text" name="" value="" placeholder="username"/></td>
+                </tr>
+                <tr>
+                    <td> Username: </td>
+                    <td> <input type="text" name="" value="" placeholder="username"/></td>
+                </tr>
+                <tr>
+                    <td> Email: </td>
+                    <td> <input type="email" name="" value="" placeholder="email"/></td>
+                </tr>
+                <tr>
+                    <td> Password: </td>
+                    <td> <input type="password" name="" value="" placeholder="password"/></td>
+                </tr>
+                <tr>
+                    <td>Confirm Password: </td>
+                    <td> <input type="password" name="" value="" placeholder="password"/></td>
+                </tr>
+
+                <tr>
+                   
+                        
+                     <td>Gender</td> 
+                       <td>
+                        <input type="radio" name="">Male
+                        <input type="radio" name="">Female
+                        <input type="radio" name="">Others
+ 
+                    
+
+                </tr>
+                <tr>
+                    <td>DOB</td>
+                    <td><input type="date"/></td>
+                </tr>
+                
+                    <td> 
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                       
+                        <input type="submit" name="" value="Submit">
+                        <input type="reset" name="" value="Reset">
+                    </td>
+                </tr>
+            </table> 
+        </fieldset>
+    </form>
+         </td>
+      </tr>
+
+      <tr align="center">
+        <td>
+          <footer style="margin: 10px">Copyright &copy; 2017</footer>
+        </td>
+      </tr>
+
+    </table>
+
+  </body>
+</html>
+
+<?php
+
+?>
